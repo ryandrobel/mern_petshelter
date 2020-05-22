d87607fc421dc34e154b0eac0974da15007ed979
Ryan Drobel <ryandrobel@gmail.com>
2020-05-22 15:53:32 -0500

    run build

---
+++ client/build/precache-manifest.d27918c6c7c6dcc62b53d37601c78bb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac164ea9ff22e94ed26df19cfd1a26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0486a55533129187326",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6c24f36f7deb8b77cc9a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0486a55533129187326",
+    "url": "/static/js/2.802f19aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.802f19aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c24f36f7deb8b77cc9a",
+    "url": "/static/js/main.7a7d38df.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
